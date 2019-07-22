a3ed87c5bc9cd16d1b27f59e0157c3a4de6670e3
LockeDerBoss <jonaspfeiffer15@gmail.com>
2019-07-22 10:29:26 +0200

    hey

---
+++ Uebungen/jajaja.py
@@ -0,0 +1,13 @@
+print("Wie alt bist du?")
+name = input ("Wie heißt du?")
+alter = int (input("Wie alt bist du?"))
+print(name)
+print(alter)
+
+if alter >=10;
+print ("Sie sind älter als 10")
+elif alter <= 10;
+
+Produkte = ("apfel", "Banane", "Birne", "Ananas")
+#print (Produkte(-31))
+woerterbuch = ("Apfel":1, "Birne":2, "Banane":34)
