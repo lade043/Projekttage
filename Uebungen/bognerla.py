941d7390ed59150582a468853ca68c5d78e0ae88
TimMcCool <timkrome2006@hotmail.com>
2019-07-22 11:28:35 +0200

    Hi!

--- Uebungen/bognerla.py
+++ Uebungen/bognerla.py
@@ -4,7 +4,7 @@ alter = int(input("Wie alt bist du? "))
 print(name)
 print(alter)
 print("Name: " + name + " Alter: " + str(alter))
-print("Dein halbes Alter ist: " + str(alter / 2))
-vier = (2 + 2) * 2
-print(vier)
-
+counter = 0
+while counter < 10:
+    print (counter)
+    counter = counter + 1
