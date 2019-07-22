b462ea01cbe9b33d3dd5ba6c55906bdfdf361337
beyerju <beyer-julian@gmx.de>
2019-07-22 09:29:02 +0200

    jdsf

---
+++ Uebungen/Test 2.0.py
@@ -0,0 +1,21 @@
+# print("Wie alt bist du?")
+# # name = (input("Wie ist dein Name"))
+# # alter = int(input("Wie alt bist du?"))
+# # print(name)
+# # print(alter)
+# #
+# # if alter >= 18:
+# #     print("Sie sind älter als 17")
+# # elif alter <= 10:
+# # print("Du bist jünger als 11")
+# #
+# # else:
+# Produkte = ["Apfel","Birne","Banane","Ananas"]
+# woerterbuch = {"Apfel":1, "Birne":2, "Banane":34}
+# for produkt in Produkte:
+#     print (produkt)
+
+zaehler = 0
+while True:
+    print (zaehler)
+    zaehler = zaehler + 1
