ff106cad4c0162aa505c2b04f779b11c945ef064
bognerla <lars.bogner@outlook.de>
2019-07-22 10:22:32 +0200

    edited test.txt

--- Uebungen/bognerla.py
+++ Uebungen/bognerla.py
@@ -1,10 +1,25 @@
-print("Projekttage")
-name = input("Was ist dein Name? ")
-alter = int(input("Wie alt bist du? "))
-print(name)
-print(alter)
-print("Name: " + name + " Alter: " + str(alter))
-print("Dein halbes Alter ist: " + str(alter / 2))
-vier = (2 + 2) * 2
-print(vier)
+# print("Projekttage")
+# name = input("Was ist dein Name? ")
+# alter = int(input("Wie alt bist du? "))
+# print(name)
+# print(alter)
+#
+# if alter >= 18:
+#     print("Sie sind älter als 17")
+# elif alter <= 10:
+#     #Du bist jünger als 11
+#     print("Du bist jünger als 11")
+#
+# else:
+#     print("Du bist zwischen 11 und 17 Jahre alt")
 
+Produkte = ["Apfel", "Birne", "Banane", "Ananas"]
+#print(Produkte[-3])
+woerterbuch = {"Apfel":1, "Birne":2, "Banane":34}
+#print(woerterbuch["Banane"])
+#for produkt in Produkte:
+#    print(produkt)
+zaehler = 0
+while True:
+    print(zaehler)
+    zaehler = zaehler + 1
