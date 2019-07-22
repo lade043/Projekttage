9f617d48362fe99068fdafb4a0dfd08117c94351
bognerla <lars.bogner@outlook.de>
2019-07-22 08:47:54 +0200

    edited test.txt

---
+++ Uebungen/bognerla.py
@@ -0,0 +1,10 @@
+print("Projekttage")
+name = input("Was ist dein Name? ")
+alter = int(input("Wie alt bist du? "))
+print(name)
+print(alter)
+print("Name: " + name + " Alter: " + str(alter))
+print("Dein halbes Alter ist: " + str(alter / 2))
+vier = (2 + 2) * 2
+print(vier)
+
