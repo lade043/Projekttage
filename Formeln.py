1d8b1409d50a6cf3686da29fbd979d7cfcc6371b
Kommindiewhatsappgruppe <Yannick.Scheiwe@web.de>
2019-07-23 09:19:34 +0200

    ggg

--- Formeln.py
+++ Formeln.py
@@ -8,4 +8,6 @@ Formeln = [k.Formel('Kraftformel', i=k.Groesse('F', 'N', False), a=k.Groesse('m'
            k.Formel('Gleichm. beschl. Bew.', i=k.Groesse('s', 'm', False), a=k.Groesse(None, None, True, 0.5),
                     b=('a', 'm/s^2', False), c=k.Groesse('t', 's', False), z=konstante_eins, x=konstante_eins,
                     y=k.Groesse(None, None, True, 2)),
-           ]
+           k.Formel("Geschwindigkeit", i=k.Groesse ('v', 'm/s',False), a=k.Groesse('s', 'm', False),
+                    b=k.Groesse ('t','s', False), z= konstante_eins, y= konstante_eins, x=k.Groesse(None,None,True,-1))]
+
