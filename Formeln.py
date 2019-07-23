779ea729a7aeafdbf27386584cbda2b9756c19cf
Lars Bogner <lars.bogner@outlook.de>
2019-07-23 10:48:36 +0200

    merged

--- Formeln.py
+++ Formeln.py
@@ -1,5 +1,7 @@
 import Klassen as k
 
-Formeln = [k.Formel('Kraftformel', i=k.Groesse('F', 'N', False), a=k.Groesse('m', 'kg', False),
+konstante_eins = k.Groesse(None, None, True, 1)
+
+Formeln = [k.Formel('Kraftformel', k.Groesse('F', 'N', False), a=k.Groesse('m', 'kg', False),
                     b=k.Groesse('a', 'm/s²', False), z=k.Groesse(None, None, True, 1), y=k.Groesse(None, None, True, 1),
-                    x=k.Groesse(None, None, True, ))]
+                    x=k.Groesse(None, None, True, 1))]
