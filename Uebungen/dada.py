cf97961f8e27afa0f3fd1e77b7ecf5a46989795c
Spongebob-Schwammkopf <materntim1@gmail.com>
2019-07-23 06:57:53 +0200

    rdrdrdrzzttuuvuctgutgcoolüjfeopwjfpiarohgoiprhtj5ziohtriooirhiodhäaoihoiahroiäaoihg

--- Uebungen/dada.py
+++ Uebungen/dada.py
@@ -1,10 +1,12 @@
 name = input("Was ist dein name")
 alter = input ("Wie alt bist du???")
 Wohnort = input ("Wo wohnst du???")
-name = ("Tim")
+name = ("tim")
 alter = (13)
-Wohnort= ("Waldrennach")
-print (name + str(alter)+ Wohnort)
+Wohnort = ("Waldrennach")
+print (name)
+print(alter)
+print(Wohnort)
 print ("Dein halbes alter ist:" + str (alter /2))
 if alter >=18:
      print ("Du bist älter  als 17")
@@ -12,6 +14,9 @@ elif alter <=10:
      print ("Du bist jünger  als 11")
 else:
      print ("Du bist jünger als 18 und älter als 11")
+while True:
+    zaehler =0
+    zaehler = zaehler + 1
 
 
 
