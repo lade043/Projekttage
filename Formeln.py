bf5282fcb67e9b89c393815c3a3ec62535ac6075
michelar <micharthur2003@gmail.com>
2019-07-24 10:22:37 +0200

    ruweigfiwgfihuidfsp98uf

--- Formeln.py
+++ Formeln.py
@@ -8,7 +8,7 @@ Formeln = [k.Formel('Kraftformel', "F=m*a", i=k.Groesse('F', 'N', False), a=k.Gr
            k.Formel('Gleichm. beschl. Bew.', i=k.Groesse('s', 'm', False), a=k.Groesse(None, None, True, 0.5),
                     b=('a', 'm/s^2', False), c=k.Groesse('t', 's', False), z=konstante_eins, x=konstante_eins,
                     y=k.Groesse(None, None, True, 2)),
-           k.Formel('Gewichtskraft', i=k.Groesse('Fg', 'N',False), a=k.Groesse(None,None,True,9.81),b=('h','m',False),
+           k.Formel('Gewichtskraft',"Fg=9,81*h", i=k.Groesse('Fg', 'N',False), a=k.Groesse(None,None,True,9.81),b=('h','m',False),
                     c=konstante_eins, z=konstante_eins,y=konstante_eins,x=konstante_eins),
            k.Formel('Spannenergie', i=k.Groesse('Espann','J', False), a=k.Groesse(None, None, True, 0.5),
                     b=k.Groesse('D', 'N/m', False), c=k.Groesse('s', 'm', False), x=konstante_eins, y=konstante_eins,
